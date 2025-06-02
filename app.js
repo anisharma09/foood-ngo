436fb44f8e047bdf5387b3c030ff66fd7160eef6
anisharma09 <153643256+anisharma09@users.noreply.github.com>
2025-06-02 14:29:31 +0530

    Update app.js

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
  
 app.use(
   cors({
-    origin: 'http://localhost:5173',  
+    origin: 'https://food-front-mauve.vercel.app',  
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true, // Allow credentials
   })
